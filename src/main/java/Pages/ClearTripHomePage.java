4777d35bf8fee69ed0483535de405dafb6ff4587
AjayDevarasetty <ajaydask3659@gmail.com>
2018-12-11 22:15:39 +0530

    Home Page description

--- src/main/java/Pages/ClearTripHomePage.java
+++ src/main/java/Pages/ClearTripHomePage.java
@@ -1,4 +1,15 @@
 package Pages;
 
+/*
+This class is responsible for opening the Home Page
+It is the common method for opening the
+SignIn Page
+Flight Booking Page
+Hotel Booking Page
+*/
+
+
+
+
 public class ClearTripHomePage {
 }
